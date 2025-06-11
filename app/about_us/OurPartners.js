6240307a0f4d4f458a2c96843425fdf43833ad48
Bayram Erdem <bayram9erdem@gmail.com>
2025-06-11 16:11:29 +0200

    Refactor Our Partners

--- app/about_us/OurPartners.js
+++ app/about_us/OurPartners.js
@@ -1,8 +1,8 @@
 import styles from "./page.module.css";
-  // TASK - React 1 week 1
-  // Create the "Our Partners section"
-  // Use the descriptions provided in /app/about_us/README.md
-  // Some inspiration ideas found in /data/inspiration_about_us
+// TASK - React 1 week 1
+// Create the "Our Partners section"
+// Use the descriptions provided in /app/about_us/README.md
+// Some inspiration ideas found in /data/inspiration_about_us
 
 export const OurPartners = () => {
   const partnerLogos = [
@@ -16,23 +16,23 @@ export const OurPartners = () => {
     "sodexo-logo.png",
   ];
 
-return (
+  return (
     <div className={styles.ourPartnersSection}>
-        <p className={styles.partnerIntro}>
-            We collaborate with some of the most respected names in the space and
-            technology industries to make every journey extraordinary.
-        </p>
+      <p className={styles.partnerIntro}>
+        We collaborate with some of the most respected names in the space and
+        technology industries to make every journey extraordinary.
+      </p>
 
-        <div className={styles.partnerGrid}>
-            {partnerLogos.map((logo, index) => (
-                <img
-                    key={index}
-                    src={`/${logo}`}
-                    alt={`Partner ${index + 1}`}
-                    className={`${styles.partnerLogos} ${styles[`delay${index}`]}`}
-                />
-            ))}
-        </div>
+      <div className={styles.partnerGrid}>
+        {partnerLogos.map((logo, index) => (
+          <img
+            key={index}
+            src={`/${logo}`}
+            alt={`Partner ${index + 1}`}
+            className={`${styles.partnerLogos} ${styles[`delay${index}`]}`}
+          />
+        ))}
+      </div>
     </div>
-);
+  );
 };
