e8c44089d367fe14b083e7c6f999484866786117
Bayram Erdem <bayram9erdem@gmail.com>
2025-06-18 14:12:50 +0200

    Included target and rel attributes for social media links

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -27,56 +27,61 @@ export const Footer = () => {
       </div> */}
       {/* Docs for the Link: https://nextjs.org/docs/pages/api-reference/components/link */}
       {/* TASK - React 1 week 1 */}
-      
-      <div className={styles.footerLinks}>
-        <h3>Follow us</h3>
-        <ul className={styles.footerList}>
-          <li>
-            <a href="https://facebook.com"></a>
-            <img
+            
+            <div className={styles.footerLinks}>
+            <h3>Follow us</h3>
+            <ul className={styles.footerList}>
+            <li>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <img
               src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png"
               alt="Facebook"
               style={{ width: 20, height: 20, marginRight: 8 }}
-            />
-            Facebook
-          </li>
-          <li>
-            <a href="https://instagram.com"></a>
-            <img
+              />
+              Facebook
+            </a>
+            </li>
+            <li>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <img
               src="https://static.vecteezy.com/system/resources/previews/018/930/415/non_2x/instagram-logo-instagram-icon-transparent-free-png.png"
               alt="Instagram"
               style={{ width: 20, height: 20, marginRight: 8 }}
-            />
-            Instagram
-          </li>
-          <li>
-            <a href="https://tiktok.com"></a>
-            <img
+              />
+              Instagram
+            </a>
+            </li>
+            <li>
+            <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">
+              <img
               src="https://img.freepik.com/premium-vector/tiktok-app-icon-logo-social-media-engagement_1293861-103.jpg"
               alt="Tiktok"
               style={{ width: 20, height: 20, marginRight: 8 }}
-            />
-            TikTok
-          </li>
-          <li>
-            <a href="https://linkedin.com"></a>
-            <img
+              />
+              TikTok
+            </a>
+            </li>
+            <li>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1024px-LinkedIn_icon.svg.png"
               alt="LinkedIn"
               style={{ width: 20, height: 20, marginRight: 8 }}
               />
-            LinkedIn
-          </li>
-          <li>
-            <a href="https://google.com"></a>
-            <img
+              LinkedIn
+            </a>
+            </li>
+            <li>
+            <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+              <img
               src="https://static-00.iconduck.com/assets.00/google-icon-2048x2026-etldt9h8.png"
               alt="Google"
               style={{ width: 20, height: 20, marginRight: 8 }}
-            />
-            Google
-          </li>
-          {/* TASK - React 1 week 2 */}
+              />
+              Google
+            </a>
+            </li>
+            {/* TASK - React 1 week 2 */}
           {/* Create a <SocialMediaItem /> component and replace all of the list items! */}
           {/* it should accept the following props */}
           {/* url, title, icon */}
